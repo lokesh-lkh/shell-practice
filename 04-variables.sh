f511c9a554a2e907c0153d6b01db9ce075dd50ed
lokesh <lokesh.theofficial@gmail.com>
2026-07-29 22:33:23 +0530

    actions

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,6 +7,3 @@ echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I'm fine . Thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. What are you learning?"
 echo "$PERSON2:: I'm learning shell . what about you?"
-
-
-
